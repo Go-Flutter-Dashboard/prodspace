115e7a29c9614b7dbe5816fbf9d1f02ec3ddc5b4
damir <damir.sadykov0407@gmail.com>
2025-07-16 13:56:03 +0300

    feat: Add class for app theme

---
+++ frontend/lib/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurpleAccent,
+      brightness: Brightness.light,
+    ),
+    useMaterial3: true,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurpleAccent,
+      brightness: Brightness.dark,
+    ),
+    useMaterial3: true,
+  );
+
+  static ThemeMode thememode = ThemeMode.system;
+
+  static setThemeMode(ThemeMode newTM) {
+    thememode = newTM;
+  }
+}
