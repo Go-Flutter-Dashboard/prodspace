66a7f87223586290a56006fea1313e99889d8d6e
SamuelAnton <SamuelAnton@users.noreply.github.com>
2025-07-17 04:26:21 +0000

    deploy: 4f19c8b0b6c2b988e7c0de2adbb7f35600814e5c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "version.json": "09727e833c95252475d99586a7f3be48",
 "manifest.json": "30a3803affc51b9b46a51dfd2b75bc8d",
-"flutter_bootstrap.js": "90e3f7d12eb0f0f256d9360ec9c8c87f",
+"flutter_bootstrap.js": "d7f591f4f7b39a5a52a7ab0bd6e4e13f",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
